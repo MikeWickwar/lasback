fe9abde967d40569b719eefb1a245382274c76c5
Mike Wickwar <mike.wickwar@gmail.com>
2016-09-10 13:02:45 -0600

    working seed for hotels

--- migrations/20160909162046_create_hotels.js
+++ migrations/20160909162046_create_hotels.js
@@ -2,12 +2,12 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('hotels', function (table) {
     table.increments();
     table.string('name');
-    table.real('lat');
-    table.real('lng');
+    table.string('lat');
+    table.string('lng');
     table.string('weight');
     table.integer('stars');
     table.string('desc');
-    table.string('isDowntown');
+    table.boolean('isDowntown');
   });
 };
 
