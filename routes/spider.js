c71fccb655e938bc5c2c188137f68e1c514258c3
Mike Wickwar <mike.wickwar@gmail.com>
2016-12-05 08:56:08 -0700

    added spider, super aweseome

---
+++ routes/spider.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+
+//get spider htels listening
+router.get('/hotel_names', function(req, res, next) {
+
+  var pageToVisit = "https://www.vegas.com/resorts/";
+  console.log("Visiting page " + pageToVisit);
+  request(pageToVisit, function(error, response, body) {
+     if(error) {
+       console.log("Error: " + error);
+     }
+     // Check status code (200 is HTTP OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode === 200) {
+       // Parse the document body
+       res.json({'hotels': {hotels: body}});
+
+       var $ = cheerio.load(body);
+       console.log("hotels?:  " + $('li.offer-item').text());
+     }
+  });
+
+});
+
+
+
+module.exports = router;
